f98aade4dd7bbb02ccc3be105121bd452607dda3
Rocky islam <mohammadrockyislam01@gmail.com>
2025-11-17 15:33:15 +0600

    problem-4 done

--- solution.ts
+++ solution.ts
@@ -48,11 +48,33 @@ class Person {
   }
 }
 
-const person1 = new Person("John Doe", 30);
-console.log(person1.getDetails());
+// const person1 = new Person("John Doe", 30);
+// console.log(person1.getDetails());
 
-const person2 = new Person("Alice", 25);
-console.log(person2.getDetails());
+// const person2 = new Person("Alice", 25);
+// console.log(person2.getDetails());
 
-const person3 = new Person("Rocky", 26);
-console.log(person3.getDetails());
+// const person3 = new Person("Rocky", 26);
+// console.log(person3.getDetails());
+
+
+//? -------Problem-4--------------
+
+type Item = {
+    title: string;
+    rating: number;
+}
+
+const filterByRating = (item: Item[]) =>{
+    return item.filter(item => item.rating >=4)
+}
+
+const books = [
+  { title: 'Book A', rating: 4.5 },
+  { title: 'Book B', rating: 3.2 },
+  { title: 'Book C', rating: 5.0 },
+  { title: 'Book d', rating: 4.0 },
+  { title: 'Book e', rating: 3.9 },
+];
+
+console.log(filterByRating(books));
