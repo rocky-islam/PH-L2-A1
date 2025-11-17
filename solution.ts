52e10219bd3ba2fe41adf72e1235c67610174a2f
Rocky islam <mohammadrockyislam01@gmail.com>
2025-11-17 13:03:32 +0600

    3rd problem solved

--- solution.ts
+++ solution.ts
@@ -1,39 +1,58 @@
 //?--------- problem-1-------------
 
-const formatValue = (value: string | number | boolean) : string | number | boolean =>{
-    if(typeof value === "string"){
-        return value.toUpperCase();
-    }
-    else if(typeof value === "number"){
-        return value*10;
-    }
+const formatValue = (
+  value: string | number | boolean
+): string | number | boolean => {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  } else if (typeof value === "number") {
+    return value * 10;
+  } else if (typeof value === "boolean") {
+    return !value;
+  }
 
-    else if(typeof value === "boolean"){
-        return !value;
-    }
-
-    return value;
-}
+  return value;
+};
 
 // console.log(formatValue('rocky'));
 // console.log(formatValue(1));
 // console.log(formatValue(false));
 
-
-
 //?-----------problem-2--------------
 
-const getLength = (value: string | number[]):number =>{
-    if(typeof value === "string"){
-        return value.length;
-    }
+const getLength = (value: string | number[]): number => {
+  if (typeof value === "string") {
+    return value.length;
+  } else if (Array.isArray(value)) {
+    return value.length;
+  }
 
-    else if(Array.isArray(value)){
-        return value.length;
-    }
+  return value;
+};
 
-    return value;
+// console.log(getLength('typescript'));
+// console.log(getLength([10, 20, 30, 40]));
+
+//?---------- Problem-3--------------
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails(): string {
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
 }
 
-console.log(getLength('typescript'));
-console.log(getLength([10, 20, 30, 40]));
+const person1 = new Person("John Doe", 30);
+console.log(person1.getDetails());
+
+const person2 = new Person("Alice", 25);
+console.log(person2.getDetails());
+
+const person3 = new Person("Rocky", 26);
+console.log(person3.getDetails());
